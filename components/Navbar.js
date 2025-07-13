42b5e1cf1a149c75b280f75a502d76029e2fdade
awaisk3 <awais.kh.3366@gmail.com>
2025-07-13 21:35:55 +0500

    navbar F

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 
 export default function Navbar() {
