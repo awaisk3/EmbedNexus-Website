e48bb72c483c2bea3af2fdd183dd672d56f8e800
awaisk3 <awais.kh.3366@gmail.com>
2025-07-13 21:33:48 +0500

    B

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 import { Mail, Phone } from "lucide-react"; // Lucide icons
 
